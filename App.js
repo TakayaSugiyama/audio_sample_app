11bd7d14c956af168443a813f18b6f3bcdfe5cd6
TakayaSugiyama <t-sugiyama@ruby-dev.jp>
2020-03-24 16:37:50 +0900

    init

--- App.js
+++ App.js
@@ -1,19 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Audio } from "expo-av";
 
-export default function App() {
+const App = () => {
+  useEffect(() => {
+    //ユーザーからマイクの使用許可を得る
+    Audio.requestPermissionsAsync();
+    Audio.setAudioModeAsync({
+      playsInSilentModeIOS: true,
+      allowsRecordingIOS: true
+    });
+  }, []);
+
+  const handlePress = async () => {
+    if (!(await Audio.getPermissionsAsync())) {
+      //マイクの使用許可が無い場合は弾く
+      alert("マイクの使用許可がありません");
+      return;
+    }
+
+    const recording = new Audio.Recording();
+    try {
+      await recording.prepareToRecordAsync(
+        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+      //録音をスタートする
+      await recording.startAsync();
+      console.log(recordingInstance.getStatusAsync());
+      alert("スタートします");
+    } catch (error) {
+      console.log(error.message);
+      alert("録音に失敗しました");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={handlePress}>
+        <Text>音声を録音する</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
+
+export default App;
--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 3. 2 で作成したオーディオファイルを Google Cloud functionni 送る。
 4. オーディオの中身がテキストになって帰ってくる.
 5. オーディオファイルを削除する。
+
+https://fostermade.co/blog/making-speech-to-text-work-with-react-native-and-expo
+
+1. Audio ライブラリ
+2. Audio ライブラリ
+3. 作成 FileSystem ライブラリ
+4. 未定
+5. 削除 FileSystem ライブラリ
